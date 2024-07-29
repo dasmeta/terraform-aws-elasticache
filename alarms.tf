5b94d6299495b446ff66613f2e1d4bd75b01c1ff
Aram Karapetyan <aram@dasmeta.com>
2024-07-29 12:13:32 +0400

    fix(DMVP-4635): Change module structure move alarms into submodule

--- alarms.tf
+++ alarms.tf
@@ -1,6 +1,8 @@
 module "alarms" {
   source = "./modules/alarms/"
 
+  count = try(var.alarms.enabled, false) ? 1 : 0
+
   name                  = var.name
   alarms                = var.alarms
   member_clusters_count = local.member_clusters_count
